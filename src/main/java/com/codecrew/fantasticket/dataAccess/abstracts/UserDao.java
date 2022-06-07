9104968e4ffceb8524650eb7ea6af8cafe94a02c
Zehra Nur Yıldırım <zehranuryi9@gmail.com>
2022-06-07 15:21:53 +0300

    add notification entity

--- src/main/java/com/codecrew/fantasticket/dataAccess/abstracts/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.codecrew.fantasticket.dataAccess.abstracts;
-
-import com.codecrew.fantasticket.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDao extends JpaRepository<User, Integer> {
-    User findByEmail(String email);
-}
--- src/main/java/com/codecrew/fantasticket/entity/Notification.java
+++ src/main/java/com/codecrew/fantasticket/entity/Notification.java
@@ -1,4 +1,73 @@
 package com.codecrew.fantasticket.entity;
 
-public class Notification {
+import com.codecrew.fantasticket.entity.base.BaseEntity;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notifications")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Notification extends BaseEntity {
+
+    @Column(name = "icon")
+    @JsonProperty(value = "icon")
+    @NotEmpty
+    private String icon;
+
+    @Column(name = "title", nullable = false)
+    @JsonProperty(value = "title")
+    @NotEmpty
+    private String title;
+
+    @Column(name = "message", nullable = false)
+    @JsonProperty(value = "message")
+    @NotEmpty
+    private String message;
+
+    @Column(name = "date", nullable = false)
+    @JsonProperty(value = "date")
+    @NotNull
+    private LocalDateTime date;
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
 }
